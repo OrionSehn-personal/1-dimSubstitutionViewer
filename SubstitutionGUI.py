650a62bd988688f45227d2f4b7044b6b8cf3ff8c
sehno <sehno@cs.macewan.ca>
2021-05-09 22:02:14 -0600

    Proper colors for up to size 5 substitutions

--- SubstitutionGUI.py
+++ SubstitutionGUI.py
@@ -7,7 +7,7 @@ from Substitution import *
 sub = {"a":"ab",
 	   "b":"a" }
 pfEigenVector = pfEigenVal(sub)
-result = Substitution(sub, "a", 7)
+result = Substitution(sub, "a", 9)
 segments = []
 xvalue = 0
 keys = list(sub.keys())
@@ -16,15 +16,24 @@ for segment in result:
 	xvalue += pfEigenVector[keys.index(segment),0]
 	newSegment.append((xvalue, 0))
 	segments.append(newSegment)
+
+
+
+
 print(pfEigenVector)
 print(segments)
 print(result)
+colorlist = ["#e89f73", "#4c91d1", "#c28897", "#c4e0ef", "#a0a5b1"]
+c = []
+for segment in result:
+	c.append(colorlist[keys.index(segment)])
 
 
 
-c = np.array([(1,0,0,1),(0,1,1,1)])
 
-linecol = mc.LineCollection(segments, linewidths=8, colors=c)
+print(c)
+
+linecol = mc.LineCollection(segments, linewidths=15, colors=c)
 fig, ax = plt.subplots()
 ax.add_collection(linecol)
 ax.margins(0.1)
