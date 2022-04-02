4241224ff6eb9bf359c42ce68439650c3ccaa145
OrionSehn <orion.sehn@gmail.com>
2022-04-02 15:15:13 -0600

    added references, updated .gitignore

--- streamlit_sub.py
+++ streamlit_sub.py
@@ -222,3 +222,34 @@ else:
         fig.colorbar(img)
         st.pyplot(fig)
 
+
+st.markdown('''---''')
+st.caption("Repository availible at: https://github.com/funmaster524/1-dimSubstitutionViewer.git")
+st.caption(
+    '''
+    References:
+
+[1] Baake M., Grimm U, Aperiodic Order: Volume 1, A Mathematical Invitation ,
+Cambridge University Press, 2013.
+
+[2] Baake M., Grimm U., The singular continuous diffraction measure of the Thue-Morse
+chain, J. Phys. A: Math. Theor. 41, 1-6, 2008.
+
+[3] Baake M., Spindeler T. Strungaru N., Diffraction of compatible random substitutions
+in one dimension , Indag. Math. 29, 1031-10711, 2018.
+
+[4] Baake M., Strungaru N., Eberlein decomposition for PV inflation systems , preprint,
+2020.
+
+[5] Richard C., Strungaru N., Pure point diffraction and Poisson summation , Ann. Henri
+Poincare 18, 3903–3931, 2017.
+
+[6] Richard C., Strungaru N., A short guide to pure point diffraction in cut-and-project
+sets , J. Phys. A: Math. Theor. 50, 2017.
+
+[7] Strungaru N., On the Fourier analysis of measures with Meyer set support , J. Funct.
+Anal. 278, 2020.
+
+[8] D. Frettlöh, E. Harriss, F. Gähler: Tilings encyclopedia, https://tilings.math.uni-bielefeld.de/
+'''
+)
